1194da99f1b1a6b9a53205cada56facc9f04f6b1
Taavi Meinberg <taavim@tlu.ee>
2018-05-27 14:30:42 +0300

    Added basic functionality to calculators

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # t13eksamitoo
+
+## Autor: Taavi Meinberg
+**Eesmärk:** Selle projekti eesmärk on ise õppida Python Flask'i framework'i kasutamist.
+Kuna anti vabad käed, siis mõtlesin proovida midagi, mida varem ei ole üldse kasutanud.
+
--- main.py
+++ main.py
@@ -2,22 +2,19 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Pea python'i fail, mis haldab nuppude kaudu navigeerumist.
+# Saadab muutuja HTML template'ile, mis sisaldab lehe nime.
 @app.route('/', methods=['POST','GET'])
 def index():
     if request.method == "POST":
         print("post")
-        if request.form['submit'] == "adding":
-            return render_template("adding.html")
-        elif request.form['submit'] == "currency":
-            return render_template("currency.html")
+        if request.form['submit'] == "currency":
+            return render_template("currency.html", name="Valuuta")
         elif request.form['submit'] == "circle":
-            return render_template("circle.html")
+            return render_template("circle.html", name="Ringi pindala")
     return render_template("index.html")
 
-@app.route('/calculator/<calc_type>')
-def calculator(calc_type):
-    return "Hello %s" % calc_type
-
+# Käivitab app'i/serveri.
 if __name__ == "__main__":
     app.run(debug=True)
 
