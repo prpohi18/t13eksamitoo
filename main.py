cfe672ee0d6528be6c5d07903b4e7af491912779
Taavi Meinberg <taavim@tlu.ee>
2018-05-30 13:44:19 +0300

    Added database logging

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, render_template, request
 
+import sql
+import sqlite3
+#conn = sqlite3.connect('database.db')
+#conn.execute('CREATE TABLE history (calc_name TEXT, timestamp DATETIME DEFAULT CURRENT_TIMESTAMP)')
+#print("Table created successfully")
+#conn.close()
+
 app = Flask(__name__)
 
 # Pea python'i fail, mis haldab nuppude kaudu navigeerumist.
@@ -9,10 +16,29 @@ def index():
     if request.method == "POST":
         print("post")
         if request.form['submit'] == "currency":
+            conn = sqlite3.connect('database.db')
+            conn.execute("INSERT INTO history (calc_name) \
+                  VALUES ('Valuuta')")
+            conn.commit()
+            print("Records created successfully")
+            conn.close()
             return render_template("currency.html", name="Valuuta")
         elif request.form['submit'] == "circle":
+            conn = sqlite3.connect('database.db')
+            conn.execute("INSERT INTO history (calc_name) \
+                              VALUES ('Ringi pindala')")
+            conn.commit()
+            conn.close()
             return render_template("circle.html", name="Ringi pindala")
     return render_template("index.html")
+
+
+@app.route('/list')
+def list():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.execute("SELECT calc_name, timestamp from history")
+    print(cursor)
+    return render_template("list.html", rows=cursor)
 
 # Käivitab app'i/serveri.
 if __name__ == "__main__":
